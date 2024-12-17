7ace9ae4cdca4b5c2221daa1cfa30b4356ce9acf
timothy833 <ca9m8e5zy@gmail.com>
2024-12-17 18:05:16 +0800

    update 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// export default defineConfig({
+//   base: process.env.NODE_ENV === 'production' ? 'react-practice' : '/',
+//   plugins: [react()],
+// })
+
+//使用vite內建功能mode指令判斷運行模式 但是需要先用回調函式導入
+export default defineConfig(({ mode })=> {
+
+  return {
+    base: mode === 'production' ? 'react-practice' : '/',
+    plugins: [react()],
+  }
 })
+
